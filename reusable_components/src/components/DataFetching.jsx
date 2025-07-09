f8e19a5df6562bdf6f744b0874b0ebf8afb08a8e
sobhiEgbaria <sobhim686@gmail.com>
2025-07-09 16:46:49 +0300

    useContext

--- reusable_components/src/components/DataFetching.jsx
+++ reusable_components/src/components/DataFetching.jsx
@@ -1,32 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const DataFetching = () => {
   const [tasks, setTasks] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMassage, setErrorMassage] = useState("");
 
   const fitchToDs = async () => {
     try {
+      setIsLoading(true);
       const res = await fetch("http://jsonplaceholder.typicode.com/todos/");
       const data = await res.json();
+      res.status === 200
+        ? setIsLoading(false)
+        : setErrorMassage("something wrong happened");
       setTasks(data);
     } catch (error) {
+      setIsLoading(false);
+      setErrorMassage("something wrong happened");
       console.log(error);
     }
   };
+  useEffect(() => {
+    fitchToDs();
+  }, []);
 
   const content = tasks.map((task) => {
     return (
-      <>
-        <h1>
-          {task.title}
-          <br />
-        </h1>
-      </>
+      <h3 key={Math.random() * 100000}>
+        {task.title}
+        <br />
+      </h3>
     );
   });
 
   return (
     <>
-      <button onClick={fitchToDs}>Fitch</button>
+      <h1>Welcome to the data fetching component</h1>
+      {/* {tasks.length === 0 && <h1>Loading....</h1>} */}
+      {isLoading && <h1>Loading....</h1>}
+      {!isLoading && errorMassage && <h1>{errorMassage}</h1>} <br />
       <br />
       {content}
     </>
